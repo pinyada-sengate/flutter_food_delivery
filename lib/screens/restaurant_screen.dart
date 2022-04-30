68d0fd552b98af3ee5dffc02598e9ca60ec7329a
Pizza <pinyadapizza@gmail.com>
2022-04-30 16:17:42 +0700

    grid view menu

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -14,6 +14,91 @@ class RestaurantScreen extends StatefulWidget {
 }
 
 class _RestaurantScreenState extends State<RestaurantScreen> {
+  _buildMenuItem(Food menuItem) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(menuItem.imageUrl),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+          ),
+          Container(
+            height: 175.0,
+            width: 175.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.black.withOpacity(0.3),
+                  Colors.black87.withOpacity(0.3),
+                  Colors.black54.withOpacity(0.3),
+                  Colors.black38.withOpacity(0.3),
+                ],
+                stops: [
+                  0.1,
+                  0.4,
+                  0.6,
+                  0.9,
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 65.0,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  menuItem.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+                Text(
+                  '\$${menuItem.price}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 10.0,
+            right: 10.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.add),
+                iconSize: 30.0,
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -150,9 +235,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
               crossAxisCount: 2,
               children: List.generate(widget.restaurant.menu.length, (index) {
                 Food food = widget.restaurant.menu[index];
-                return Center(
-                  child: Text(food.name),
-                );
+                return _buildMenuItem(food);
               }),
             ),
           ),
